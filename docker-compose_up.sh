b36362c02738fc64251bccbe6541c4313eb348d2
Haolun Zhang <chshawkn@users.noreply.github.com>
2019-11-12 12:22:51 +1300

    update default settings

--- docker-compose_up.sh
+++ docker-compose_up.sh
@@ -1,4 +1,10 @@
 
+export LOGGER_LEVEL="INFO"
+#export LOGGER_LEVEL="TRACE"
+export ELASTICSEARCH_THREAD_POOL_WRITE_SIZE="3"
+export ES_JAVA_OPTS="-Xms5120m -Xmx5120m"
+export MEM_LIMIT="10g"
+
 docker-compose down -v
 
 read -p "Do you want clear data: " -n 1 -r
