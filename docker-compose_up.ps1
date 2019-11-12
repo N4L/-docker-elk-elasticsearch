89820d6373cd7f539d9abfb894f7b4e61b00c91e
Haolun Zhang <chshawkn@users.noreply.github.com>
2019-11-12 20:50:57 +1300

    update windows start script

--- docker-compose_up.ps1
+++ docker-compose_up.ps1
@@ -3,6 +3,12 @@ $p = (pwd) -replace "([A-Z]{1}):\\","/`$1/"
 $env:PWD = ($p.substring(0,2).ToLower() + $p.substring(2)).replace("\", "/")
 Get-ChildItem Env:PWD
 
+$env:LOGGER_LEVEL = "INFO"
+#$env:LOGGER_LEVEL = "TRACE"
+$env:ELASTICSEARCH_THREAD_POOL_WRITE_SIZE = "3"
+$env:ES_JAVA_OPTS = "-Xms5120m -Xmx5120m"
+$env:MEM_LIMIT = "10g"
+
 $env:IMAGE_ARG_ES_IMAGE_NAME = 'elasticsearch'
 
 docker-compose down -v
